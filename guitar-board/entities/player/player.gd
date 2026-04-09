92605c2a30ac4d41b53e5277e10c60101834be54
Swarry123 <137361549+Swarry123@users.noreply.github.com>
2026-04-09 12:27:06 +1000

    Update player.gd

--- guitar-board/entities/player/player.gd
+++ guitar-board/entities/player/player.gd
@@ -26,6 +26,7 @@ func _physics_process(delta: float) -> void:
 	
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
 		
 		if collision.get_collider().name == "Mob":
 			get_hit()
