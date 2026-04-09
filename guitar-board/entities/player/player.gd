642fec1d536477639de9e7e8b94332ce0e54c22a
Swarry123 <137361549+Swarry123@users.noreply.github.com>
2026-04-09 12:22:02 +1000

    player can get hit

--- guitar-board/entities/player/player.gd
+++ guitar-board/entities/player/player.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 @export var speed = 400.0
 @export var rot_speed = 5.0
 var rot_vel = 0
-var health = 50
+var health = 100
 
 func get_input():
 	# Rotation
@@ -17,9 +17,16 @@ func get_input():
 
 func get_hit():
 	health-=2
-	
+
+
 	
 func _physics_process(delta: float) -> void:
 	get_input()
 	move_and_slide()
+	
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		
+		if collision.get_collider().name == "Mob":
+			get_hit()
 	
