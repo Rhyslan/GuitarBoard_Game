9ac8778968e1f4e5e712462a0f8f8e54aedfe8f6
Rhyslan <79964831+Rhyslan@users.noreply.github.com>
2026-04-15 15:08:52 +1000

    Update UI display
    Fix dash cooldown, remove action input list, add action labels, add tint to reload and slash refill

--- guitar-board/scenes/test/test.gd
+++ guitar-board/scenes/test/test.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 signal round_change(new_round)
 
-var input_list := "Actions pressed:\n"
 var mob := preload("res://entities/enemies/mob.tscn")
 
 @onready var round_num := 1
@@ -11,41 +10,6 @@ var mob := preload("res://entities/enemies/mob.tscn")
 
 
 func _process(delta: float) -> void:
-	input_list = "Actions pressed:\n"
-	# Movement
-	if Input.is_action_pressed("Up"):
-		input_list += "Move Up\n"
-	if Input.is_action_pressed("Down"):
-		input_list += "Move Down\n"
-	if Input.is_action_pressed("Left"):
-		input_list += "Move Left\n"
-	if Input.is_action_pressed("Right"):
-		input_list += "Move Right\n"
-	# Actions
-	if Input.is_action_pressed("Jump"):
-		input_list += "Jump\n"
-	if Input.is_action_pressed("Gun"):
-		input_list += "Gun attack selected\n"
-	if Input.is_action_pressed("Beam"):
-		input_list += "Beam attack selected\n"
-	if Input.is_action_pressed("Slash"):
-		input_list += "Slash attack selected\n"
-	if Input.is_action_pressed("Dash"):
-		input_list += "Dash selected\n"
-	if Input.is_action_pressed("Shield"):
-		input_list += "Shield selected\n"
-	if Input.is_action_pressed("Attack"):
-		input_list += "Activating selection\n"
-	if Input.is_action_pressed("Reload"):
-		input_list += "Reloading\n"
-	
-	# Pausing
-	if Input.is_action_just_pressed("Pause"):
-		input_list += "Game Paused\n"
-	
-	$"UI Canvas/UI/Label".text = input_list
-	#$CanvasLayer/UI/Health.text = "Health: " + str(player.health)
-	
 	# Round timer updates, after 30 seconds round updates
 	round_timer += delta
 	if round_timer >= 30:
